a32e8ea5cec087ec82663bfb59305fea46914e1c
Ishita Pradhan <ishitapradhan03072004@gmail.com>
2026-02-11 20:59:56 +0530

    Initial commit

---
+++ src/basicSorting/selectionSort.java
@@ -0,0 +1,31 @@
+package basicSorting;
+
+public class selectionSort {
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+        public static void main (String[]args){
+            int[] arr = {10, -4, 20, 1, -6, 8};
+            int n = arr.length;
+            print(arr);
+            for (int i = 0; i < n - 1; i++) {
+                int min = Integer.MAX_VALUE;
+                int minindex = -1;
+                for (int j = i; j < n; j++) {
+                    if (arr[j] < min) {
+                        min = arr[j];
+                        minindex = j;
+                    }
+                }
+                //swap arr[i] and arr[minindex]
+                int temp = arr[i];
+                arr[i] = arr[minindex];
+                arr[minindex] = temp;
+            }
+            print(arr);
+        }
+    }
+
