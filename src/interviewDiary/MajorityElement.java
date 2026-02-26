34986409c7d2642120772c652b5507d8765aae16
Ishita Pradhan <ishitapradhan03072004@gmail.com>
2026-02-26 07:52:10 +0530

    Initial commit

---
+++ src/interviewDiary/MajorityElement.java
@@ -0,0 +1,25 @@
+package interviewDiary;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int nums[] = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(MajorityElement(nums));
+    }
+
+    public static int MajorityElement(int[] nums) {
+        int cand = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                cand = nums[i];
+            }
+            if (cand == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+            return cand;
+
+    }
+}
