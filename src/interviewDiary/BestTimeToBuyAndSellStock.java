ad9fd7e04d5260aa964e8f6df8759e0a7a6f64b2
Ishita Pradhan <ishitapradhan03072004@gmail.com>
2026-03-04 19:06:13 +0530

    Initial commit

---
+++ src/interviewDiary/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,24 @@
+package interviewDiary;
+
+
+public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        int prices[]={7,1,5,4,6,4};
+        System.out.println(maxProfit(prices));
+    }
+    public static int maxProfit(int prices[]){
+        int n=prices.length;
+        int max_Profit=0;
+        int buy_price=prices[0];
+        for(int i=0;i<n;i++){
+            int curr_profit=prices[i]-buy_price;
+            if(curr_profit>max_Profit){
+                max_Profit=curr_profit;
+            }
+            if(prices[i]<buy_price){
+                buy_price=prices[i];
+            }
+        }
+        return max_Profit;
+    }
+}
