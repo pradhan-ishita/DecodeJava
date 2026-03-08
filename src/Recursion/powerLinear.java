29c6e878e234c3eaba0732e913d081f0a196e611
Ishita Pradhan <ishitapradhan03072004@gmail.com>
2026-03-08 08:49:14 +0530

    Initial commit

--- src/Recursion/powerLinear.java
+++ src/Recursion/powerLinear.java
@@ -3,14 +3,17 @@ package Recursion;
 import java.util.Scanner;
 
 public class powerLinear {
-    public static int pow(int a, int b){
-        if(a==0 && b==0){
-            System.out.println("Not Defined");
-            return -1;
-        }
+    public static int pow(int a, int b){//TC=0(b)
         if (b==0) return 1;
         return a* pow(a,b-1);
     }
+    public static int pow2(int a, int b){ //TC=0(log b)
+        if (b==0) return 1;
+        int ans=pow2(a,b/2);
+        if(b%2==0) return ans*ans;
+        else return ans*ans*a;
+    }
+
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter base: ");
