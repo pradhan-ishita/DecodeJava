a9b526cba15530e763625dc8910a0283d3cda8d1
Ishita Pradhan <ishitapradhan03072004@gmail.com>
2026-03-09 22:02:40 +0530

    Initial commit

--- src/interviewDiary/RotateArray.java
+++ src/interviewDiary/RotateArray.java
@@ -6,11 +6,9 @@ public class RotateArray {
     public static void main(String[] args) {
         int nums[]={1,2,3,4,5,6,7};
         int k=4;
-
         System.out.println(Arrays.toString(rotate(nums,k)));
     }
     public static int[]rotate (int[]nums,int k){
-
         int n=nums.length;
          k=k%n;
         reverse(nums,0,n-1);
@@ -19,7 +17,6 @@ public class RotateArray {
         return nums;
 
     }
-
     public static void reverse(int[]nums,int start,int end) {
         while (start <= end) {
             int temp = nums[start];
