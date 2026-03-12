e5ab0b11a63d564a643c3e0d2c45b0207a0668cf
Ishita Pradhan <ishitapradhan03072004@gmail.com>
2026-03-12 23:57:38 +0530

    Initial commit

---
+++ src/interviewDiary/JumpGame.java
@@ -0,0 +1,17 @@
+package interviewDiary;
+
+public class JumpGame {
+    public static void main(String[] args) {
+        int nums[]={3,2,1,0,4};
+        System.out.println(canJump(nums));
+    }
+    public static boolean canJump(int[] nums){
+        int lastpos=nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(i+nums[i]>=lastpos){
+                lastpos=i
+;            }
+        }
+        return lastpos==0;
+    }
+}
