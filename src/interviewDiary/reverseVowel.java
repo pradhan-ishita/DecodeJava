2018ceb7e446bb7eb01bf40ef8006b6f50989988
Ishita Pradhan <ishitapradhan03072004@gmail.com>
2026-07-05 01:07:20 +0530

    Initial commit

---
+++ src/interviewDiary/reverseVowel.java
@@ -0,0 +1,42 @@
+package interviewDiary;
+
+public class reverseVowel {
+
+    public static void main(String[] args) {
+        System.out.println(reverseVowels("hello"));
+    }
+
+    public static String reverseVowels(String s) {
+
+        char[] ch = s.toCharArray();
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+
+            if (!isVowel(ch[start])) {
+                start++;
+            }
+            else if (!isVowel(ch[end])) {
+                end--;
+            }
+            else {
+                char temp = ch[start];
+                ch[start] = ch[end];
+                ch[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+
+        return new String(ch);
+    }
+
+    public static boolean isVowel(char ch) {
+
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+}
