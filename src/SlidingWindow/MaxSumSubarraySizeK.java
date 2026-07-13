52de9738e22d6a12772246e786ae35d3d18598bf
Ishita Pradhan <ishitapradhan03072004@gmail.com>
2026-07-13 13:14:17 +0530

    Initial commit

--- src/SlidingWindow/MaxSumSubarraySizeK.java
+++ src/SlidingWindow/MaxSumSubarraySizeK.java
@@ -5,17 +5,27 @@ public class MaxSumSubarraySizeK {
 
 
         int[] arr = {10, 20, 1, 3, -40, 80, 10};
-        int k = 3;
+        int k = 2;
         int n = arr.length;
-        int maxSum = 0;
-        for (int i = 0; i < n - k + 1; i++) {
-            int sum = 0;
-            for (int j = i; j <= i + k - 1; j++) {
-                sum += arr[j];
-            }
-            maxSum = Math.max(maxSum, sum);
-
+//        for (int i = 0; i < n - k + 1; i++) {
+//            int sum = 0;
+//            for (int j = i; j <= i + k - 1; j++) {
+//                sum += arr[j];
+//            }
+//            maxSum = Math.max(maxSum, sum);
+//
+//        }
+        int maxSum=0;
+        int i=0,j=k-1,sum=0;
+        for(int a=0;a<=k-1;a++){//k times
+            sum+= arr[a];
+        }
+        i++;j++;
+        while(j<n){//n-k times
+            sum=sum-arr[i-1]+arr[j];
+            maxSum=Math.max(maxSum,sum);
+            i++;j++;
         }
         System.out.println(maxSum);
-    }
+       }
 }
